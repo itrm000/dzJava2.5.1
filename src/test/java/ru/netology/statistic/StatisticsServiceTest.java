8d198be57b969cb34f9636cb8cd789531962ec98
Anatoly <itrm000@gmail.com>
2021-01-21 23:20:42 +0500

    third commit

---
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -0,0 +1,31 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatisticsServiceTest {
+
+
+  @Test
+  @CsvFileSource(resources = {"/data.csv"}, delimiter = '|')
+  void findMax() {
+    StatisticsService service = new StatisticsService();
+
+    long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+    long expected = 12;
+
+    long actual = service.findMax(incomesInBillions);
+
+    assertEquals(expected, actual);
+  }
+
+
+
+
+
+
+
+
+}
